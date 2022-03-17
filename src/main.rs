0af865f7cc329c85212e33771e07f924ab5c0734
kvoid <kianheitkamp@outlook.com>
2022-03-17 09:13:56 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use ui::start;
 
 const WINDOW_TITLE: druid::LocalizedString<ui::State> =
     druid::LocalizedString::new("Kian's ToolFetch");
-
+    
 fn main() {
     #[cfg(target_os = "windows")]
     std::thread::spawn(move || {
